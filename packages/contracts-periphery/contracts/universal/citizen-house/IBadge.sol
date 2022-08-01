d9c9c8e644502441499790d44975d06e5622d4df
hans <hmrtn@pm.me>
2022-08-01 12:45:52 -0400

    added: natspec desc

--- packages/contracts-periphery/contracts/universal/citizen-house/IBadge.sol
+++ packages/contracts-periphery/contracts/universal/citizen-house/IBadge.sol
@@ -3,6 +3,11 @@ pragma solidity 0.8.15;
 
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
+/**
+ * @title Badge Interface Contract
+ * @author OPTIMISM + GITCOIN
+ */
+
 interface IBadge is IERC721 {
     function mint(address) external;
 
