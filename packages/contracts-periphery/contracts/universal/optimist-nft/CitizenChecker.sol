deaa1e779f27dde97a07125ad0b153d673d25173
hans <hmrtn@pm.me>
2022-10-04 16:59:17 -0700

    fixed: citizen attestations

--- packages/contracts-periphery/contracts/universal/optimist-nft/CitizenChecker.sol
+++ packages/contracts-periphery/contracts/universal/optimist-nft/CitizenChecker.sol
@@ -12,17 +12,27 @@ contract CitizenChecker is ICitizenChecker {
     address public root;
     SocialContract public sc;
 
+    function toBool(bytes memory _b) internal pure returns (bool) {
+        return abi.decode(_b, (bool));
+    }
+
+    function toUint256(bytes memory _b) internal pure returns (uint256) {
+        return abi.decode(_b, (uint256));
+    }
+
     function isCitizen(address _who, bytes memory _proof) external view returns (bool) {
         CitizenProof memory proof = abi.decode(_proof, (CitizenProof));
         bytes memory numOpcoCitizenships = sc.attestations(root, proof.opco, keccak256("op.opco"));
-        require(proof.index < uint256(bytes32(numOpcoCitizenships)));
-        // require(
-        //     sc.attestations(
-        //         proof.opco,
-        //         _who,
-        //         keccak256(abi.encodePacked("op.opco.citizen.", proof.index))
-        //     ) == true // TODO: fix this
-        // );
+        require(proof.index < toUint256(numOpcoCitizenships));
+        require(
+            toBool(
+                sc.attestations(
+                    proof.opco,
+                    _who,
+                    keccak256(abi.encodePacked("op.opco.citizen.", proof.index))
+                )
+            )
+        );
         return true;
     }
 }
